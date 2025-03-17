075e734dbc026d767eb2306bf3cca0cc4de4338f
Sunny Kumar <sunnykumar@Sunnys-MacBook-Air.local>
2025-03-17 15:43:26 +0530

    added rightside component and made responsive

--- src/components/LeftSide/LeftSide.jsx
+++ src/components/LeftSide/LeftSide.jsx
@@ -1,7 +1,7 @@
 import React, {useState} from 'react'
 import axios from "axios"
 import "./LeftSide.css"
-// import main from "../../../src/Assets"
+
 
 
 const LeftSide = () => {
--- src/components/RightSide/RightSide.jsx
+++ src/components/RightSide/RightSide.jsx
@@ -1,10 +1,31 @@
-import React from 'react'
-import "./RightSide.css"
+import React from "react";
+import "./RightSide.css";
 
 const RightSide = () => {
-  return (
-   <div className="right">aevg</div>
-  )
-}
+  const cardData = [
+    { title: "Presentation Design", image: "/Assets/presentation.png", description: "Lorem ipsum dolor sit amet, lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet" },
+    { title: "Audio - Visual Production", image: "/Assets/audio.png", description: "Lorem ipsum dolor sit amet, lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet" },
+    { title: "Translation Services", image: "/Assets/translation.png", description: "Lorem ipsum dolor sit amet, lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet" },
+    { title: "Graphic Design", image: "/Assets/graphic.png", description: "Lorem ipsum dolor sit amet, lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet" },
+    { title: "Research & Analytics", image: "/Assets/research.png", description: "Lorem ipsum dolor sit amet, lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet" },
+    { title: "Data Processing", image: "/Assets/data.png", description: "Lorem ipsum dolor sit amet, lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet" }
+  ];
 
-export default RightSide
+  return (
+    <div className="right-side">
+      <div className="cards-container">
+        {cardData.map((card, index) => (
+          <div key={index} className="card">
+            <div className="card-header">
+              <img src={card.image} alt={card.title} className="card-logo" />
+              <h3 className="card-title">{card.title}</h3>
+            </div>
+            <p className="card-text">{card.description}</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default RightSide;
